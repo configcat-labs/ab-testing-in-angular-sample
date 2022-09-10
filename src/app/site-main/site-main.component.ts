fa29a31e62d0e7ad1e0a74689dee8514c0ef5c3f
Chavez Harris <codedbychavez@gmail>
2022-09-10 19:36:23 -0300

    Integrated ConfigCat

--- src/app/site-main/site-main.component.ts
+++ src/app/site-main/site-main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as configcat from 'configcat-js';
 
 @Component({
   selector: 'app-site-main',
@@ -6,8 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./site-main.component.css']
 })
 export class SiteMainComponent implements OnInit {
+  canShowGreenPricingBadge:boolean = false;
 
-  constructor() { }
+  constructor() {
+    let configCatClient = configcat.createClient("eJPaCHq8NEKIV0SCfou-qQ/lg7L5k7AeEu1A9P0EvB6xA");
+    configCatClient.getValueAsync("canshowgreenpricingbadge", false)
+    .then(value => {
+      this.canShowGreenPricingBadge = value;
+    })
+   }
 
   ngOnInit(): void {
   }
