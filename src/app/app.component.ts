815edf84d1c796c12aea807237a856810fd5daed
Chavez Harris <codedbychavez@gmail.com>
2025-02-25 19:49:38 -0400

    Installed Amplitude analytics SDK via `npm install`

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import {SiteHeaderComponent} from './site-header/site-header.component';
 import {SiteNavigationComponent} from './site-navigation/site-navigation.component';
 import {SiteMainComponent} from './site-main/site-main.component';
 import {SiteFooterComponent} from './site-footer/site-footer.component';
+// Import Amplitude service
 
 @Component({
   selector: 'app-root',
