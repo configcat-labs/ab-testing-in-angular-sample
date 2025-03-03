092f246b43990f7ffe2381c5727ebf070c4e46f2
Chavez Harris <codedbychavez@gmail.com>
2025-03-03 13:13:33 -0400

    Add environments

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  amplitudeApiKey: '05f096a5aee456b785c6383acdcf662f',
+  configCatSDKKey: 'configcat-sdk-1/gnLbCJ_nhUCGHl1SZNyC5Q/WqnR63aIJkeepONMnI4p5g'
+};
