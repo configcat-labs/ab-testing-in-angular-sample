69d897ad6ca68bfaa07eb0bb525ee15749500726
Chavez Harris <codedbychavez@gmail.com>
2026-02-26 16:40:17 -0400

    Minor updates

--- src/app/site-main/site-main.component.ts
+++ src/app/site-main/site-main.component.ts
@@ -1,33 +1,34 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
-import {NgClass} from '@angular/common';
-import {ConfigCatService} from '../services/configcat.service';
-import {AmplitudeService} from '../services/amplitude.service';
-import {environment} from '../../environments/environment';
+import { NgClass } from '@angular/common';
+import { ConfigCatService } from '../services/configcat.service';
+import { AmplitudeService } from '../services/amplitude.service';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-site-main',
-  imports: [
-    NgClass
-  ],
+  imports: [NgClass],
   templateUrl: './site-main.component.html',
   standalone: true,
-  styleUrl: './site-main.component.scss'
+  styleUrl: './site-main.component.scss',
 })
 export class SiteMainComponent implements OnInit {
   private configCatService = inject(ConfigCatService);
   readonly connectionState = this.configCatService.connectionState();
-  isGreenPricingBadgeEnabled = this.configCatService.getValue('greenPricingBadge', false)
+  isGreenPricingBadgeEnabled = this.configCatService.getValue(
+    'greenPricingBadge',
+    false,
+  );
 
-  constructor(
-    private amplitudeService: AmplitudeService){}
+  constructor(private amplitudeService: AmplitudeService) {}
 
   async ngOnInit() {
     this.amplitudeService.initialize(environment.amplitudeApiKey);
   }
 
-
   handleProPlanClick(badgeColor: string) {
-    this.amplitudeService.trackEvent('ProPlanClick', { badgeColor: badgeColor });
+    this.amplitudeService.trackEvent('ProPlanClick', {
+      badgeColor: badgeColor,
+    });
     window.alert(`Pro plan: ${badgeColor}`);
   }
 }
